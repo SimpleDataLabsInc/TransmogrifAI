b2e46098bc0c747f1cc9997e3876ea0ba267a1cd
Mayank <mayank@simpledatalabs.com>
2019-03-01 14:22:11 +0530

    serialize fix

--- utils/src/main/scala/com/salesforce/op/utils/json/SpecialDoubleSerializer.scala
+++ utils/src/main/scala/com/salesforce/op/utils/json/SpecialDoubleSerializer.scala
@@ -31,19 +31,25 @@
 package com.salesforce.op.utils.json
 
 import org.json4s.CustomSerializer
-import org.json4s.JsonAST.JString
+import org.json4s.JsonAST.{JDouble, JString}
 
 /**
  * Json4s serializer for marshalling special Double values: NaN, -Infinity and Infinity
  */
 // scalastyle:off
 class SpecialDoubleSerializer extends CustomSerializer[Double](_ =>
-  ({
-    case JString("NaN") => Double.NaN
-    case JString("-Infinity") => Double.NegativeInfinity
-    case JString("Infinity") => Double.PositiveInfinity
-  }, {
-    case v: Double if v.isNaN => JString("NaN")
-    case Double.NegativeInfinity => JString("-Infinity")
-    case Double.PositiveInfinity => JString("Infinity")
-  }))
+( {
+  case JString("NaN") => Double.NaN
+  case JString("-Infinity") => Double.NegativeInfinity
+  case JString("Infinity") => Double.PositiveInfinity
+  case JDouble(v) ⇒ v
+  case v ⇒ throw new Exception(s"Invalid Type to convert: ${v} to Double")
+}, {
+  case v: Double if v.isNaN => JString("NaN")
+  case Double.NegativeInfinity => JString("-Infinity")
+  case Double.PositiveInfinity => JString("Infinity")
+  case v: Double => JDouble(v)
+  case v: Int => JDouble(v)
+  case v: Long => JDouble(v)
+  case v: Float => JDouble(v)
+}))
